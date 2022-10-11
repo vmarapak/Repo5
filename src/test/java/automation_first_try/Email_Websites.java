1fc4a570a8778e96609328617b5a19aa4d794dc3
vmarapak <vmarapak@gmail.com>
2022-10-11 12:12:42 -0400

    my first automation code

--- src/test/java/automation_first_try/Email_Websites.java
+++ src/test/java/automation_first_try/Email_Websites.java
@@ -6,26 +6,27 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Social_Media {
+public class Email_Websites {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https:/www.myspace.com");
+		driver.get("http://www.gmail.com");
 		
 		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver1 = new FirefoxDriver();
-		driver1.manage().window().maximize();
-		driver1.get("https:/www.twitter.com");
-	
-	
+	WebDriver driver1 = new FirefoxDriver();
+	driver1.manage().window().maximize();
+	driver1.get("http:/www.yahoomail.com");
 	
 	driver.quit();
 	driver1.quit();
 	
-	
+		
+		
+		
 	}
 
 }
